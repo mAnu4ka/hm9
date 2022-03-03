3f88f80a27c5fa6d115e824515dfccd63d1ca874
mAnu4ka <94441221+mAnu4ka@users.noreply.github.com>
2022-03-03 12:54:26 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -222,21 +222,34 @@ let arr_products = [{
 // const newfunk = () =>{}
 let max = document.querySelectorAll('.all')
 
-const sord = (a)  => {
+const sord = (a) => {
+    let butonse = document.querySelectorAll('.btn')
+
     if (a == 'max') {
         arr_products = arr_products.sort((a, b) => {
             return a.price - b.price
         })
-    }else if (a == 'min'){
+    } else if (a == 'min') {
         arr_products = arr_products.sort((a, b) => {
             return b.price - a.price
         })
-    }else console.log('не работает');
-    CreatElement()
+    } else console.log('не работает');
+    let el = ''
+    for (const key of butonse) {
+        if (key.innerText == 'Добавлено') {
+            key.push(a)
+        } else if (a == 'arr_products') {
+            CreatElement(a)
+            return
+        } else if (a == 'star') {
+            key.push(a)
+        }
+    }
+    CreatElement(a)
 }
 
 for (const keys of max) {
-    keys.onclick  = () =>{
+    keys.onclick = () => {
         let atrs = keys.getAttribute('what')
         sord(atrs)
     }
@@ -254,10 +267,8 @@ const counOFunction = () => {
             arr_products[idx].price = Math.round(arr_products[idx].price);
         } else console.log('пока не работает');
         if (arr_products[idx].rating.count <= 0) {
-            key.innerHTML = 'p'
-            key.innerText = 'твоара нет'
-            key.classList.add('pblok')
-            key.classList.remove('but')
+            key.innerText = 'Добавлено'
+            key.classList.add('nice')
         }
     }
 }
@@ -279,11 +290,12 @@ const priceFunction = () => {
             let result2 = Math.round(result);
             arr_products[idx].price = result2
             key.append(img)
-        }else console.log(arr_products[idx].rating.count);
+        } else console.log(arr_products[idx].rating.count);
     }
-    CreatElement()
+    CreatElement(arr_products)
     finctions()
 }
+
 
 const finctions = () => {
     let butons = document.querySelectorAll('.btn')
@@ -291,71 +303,78 @@ const finctions = () => {
         let id = key.getAttribute('id')
         let idx = arr_products.indexOf(arr_products.filter(item => item.id == id)[0])
         key.onclick = () => {
-            key.classList.add('nice')
-            key.innerText = 'Добавлено'
-            arr_products[idx].rating.count -= 1
-            cart.push(arr_products[idx])
-            CreatElement()
+            if (arr_products[idx].rating.count <= 0) {
+                return
+            } else {
+                arr_products[idx].rating.count -= 1
+                cart.push(arr_products[idx])
+            }
+            if (arr_products[idx].rating.count <= 0) {
+                key.innerText = 'Добавлено'
+                key.classList.add('nice')
+            }
+            CreatElement(arr_products)
         }
     }
     counOFunction()
 }
 let greed = document.querySelector('.greed')
 let cart = []
-const CreatElement = () => {
+let star = []
+const CreatElement = (a) => {
     greed.innerHTML = ''
     let pp = document.querySelector('.pp')
     pp.innerText = cart.length
-    for (const item of arr_products) {
+    for (const item of a) {
         greed.innerHTML += ` 
-        <div class="item">
-        <div class="img">
-        <img src="./81fPKd-2AYL 1.png">
-        </div>
-        <div class="buttom defolt-with">
-        <h2>${item.title}</h2>
-        <h3>${item.description}</h3>
-        <div class="info">
-        <div class="item-info">
-        <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5 1V15.6667" stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
-                            <path
-                            d="M8.33333 3.66797H3.33333C2.71449 3.66797 2.121 3.9138 1.68342 4.35139C1.24583 4.78897 1 5.38246 1 6.0013C1 6.62014 1.24583 7.21363 1.68342 7.65122C2.121 8.0888 2.71449 8.33464 3.33333 8.33464H6.66667C7.28551 8.33464 7.879 8.58047 8.31658 9.01805C8.75417 9.45564 9 10.0491 9 10.668C9 11.2868 8.75417 11.8803 8.31658 12.3179C7.879 12.7555 7.28551 13.0013 6.66667 13.0013H1"
-                            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <p class='price'>${item.price}</p>
-                    </div>
-                    <div class="item-info">
-                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M7.10542 1L8.99199 4.82199L13.2108 5.43864L10.1581 8.41197L10.8786 12.6125L7.10542 10.6282L3.33227 12.6125L4.05271 8.41197L1 5.43864L5.21884 4.82199L7.10542 1Z"
-                            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                        <p>${item.rating.rate}</p>
-                    </div>
-                    <div class="item-info">
-                        <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 5.93266L4 2.47266" stroke="#1F86FF" stroke-linecap="round"
-                            stroke-linejoin="round" />
-                        <path
-                            d="M13 10.332V4.99863C12.9998 4.76482 12.938 4.53517 12.821 4.33274C12.704 4.13031 12.5358 3.96221 12.3333 3.8453L7.66667 1.17863C7.46397 1.06161 7.23405 1 7 1C6.76595 1 6.53603 1.06161 6.33333 1.17863L1.66667 3.8453C1.46418 3.96221 1.29599 4.13031 1.17897 4.33274C1.06196 4.53517 1.00024 4.76482 1 4.99863V10.332C1.00024 10.5658 1.06196 10.7954 1.17897 10.9979C1.29599 11.2003 1.46418 11.3684 1.66667 11.4853L6.33333 14.152C6.53603 14.269 6.76595 14.3306 7 14.3306C7.23405 14.3306 7.46397 14.269 7.66667 14.152L12.3333 11.4853C12.5358 11.3684 12.704 11.2003 12.821 10.9979C12.938 10.7954 12.9998 10.5658 13 10.332Z"
-                            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M1.17999 4.30469L6.99999 7.67135L12.82 4.30469" stroke="#1F86FF" stroke-linecap="round"
-                            stroke-linejoin="round" />
-                        <path d="M7 14.3841V7.66406" stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                        <p>${item.rating.count}</p>
-                    </div>
-                </div>
-                <button class='btn' id='${item.id}'>В избранное</button>
+            <div class="item">
+            <div class="img">
+            <img src="./81fPKd-2AYL 1.png">
             </div>
-        </div>
-        `
+            <div class="buttom defolt-with">
+            <h2>${item.title}</h2>
+            <h3>${item.description}</h3>
+            <div class="info">
+            <div class="item-info">
+            <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 1V15.6667" stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
+            <path
+            d="M8.33333 3.66797H3.33333C2.71449 3.66797 2.121 3.9138 1.68342 4.35139C1.24583 4.78897 1 5.38246 1 6.0013C1 6.62014 1.24583 7.21363 1.68342 7.65122C2.121 8.0888 2.71449 8.33464 3.33333 8.33464H6.66667C7.28551 8.33464 7.879 8.58047 8.31658 9.01805C8.75417 9.45564 9 10.0491 9 10.668C9 11.2868 8.75417 11.8803 8.31658 12.3179C7.879 12.7555 7.28551 13.0013 6.66667 13.0013H1"
+            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p class='price'>${item.price}</p>
+            </div>
+            <div class="item-info">
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+            d="M7.10542 1L8.99199 4.82199L13.2108 5.43864L10.1581 8.41197L10.8786 12.6125L7.10542 10.6282L3.33227 12.6125L4.05271 8.41197L1 5.43864L5.21884 4.82199L7.10542 1Z"
+            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p>${item.rating.rate}</p>
+            </div>
+            <div class="item-info">
+            <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 5.93266L4 2.47266" stroke="#1F86FF" stroke-linecap="round"
+            stroke-linejoin="round" />
+            <path
+            d="M13 10.332V4.99863C12.9998 4.76482 12.938 4.53517 12.821 4.33274C12.704 4.13031 12.5358 3.96221 12.3333 3.8453L7.66667 1.17863C7.46397 1.06161 7.23405 1 7 1C6.76595 1 6.53603 1.06161 6.33333 1.17863L1.66667 3.8453C1.46418 3.96221 1.29599 4.13031 1.17897 4.33274C1.06196 4.53517 1.00024 4.76482 1 4.99863V10.332C1.00024 10.5658 1.06196 10.7954 1.17897 10.9979C1.29599 11.2003 1.46418 11.3684 1.66667 11.4853L6.33333 14.152C6.53603 14.269 6.76595 14.3306 7 14.3306C7.23405 14.3306 7.46397 14.269 7.66667 14.152L12.3333 11.4853C12.5358 11.3684 12.704 11.2003 12.821 10.9979C12.938 10.7954 12.9998 10.5658 13 10.332Z"
+            stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1.17999 4.30469L6.99999 7.67135L12.82 4.30469" stroke="#1F86FF" stroke-linecap="round"
+            stroke-linejoin="round" />
+            <path d="M7 14.3841V7.66406" stroke="#1F86FF" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <p>${item.rating.count}</p>
+            </div>
+            </div>
+            <button class='btn' id='${item.id}'>В избранное</button>
+            </div>
+            </div>
+            `
     }
     finctions()
 }
 
 finctions()
 counOFunction()
-CreatElement()
+CreatElement(arr_products)
 priceFunction()
